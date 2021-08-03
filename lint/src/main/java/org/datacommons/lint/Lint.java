a2119ca5eedd44ed2d8c6268a84e71aeeb3aad39
Prashanth Radhakrishnan <shanth@google.com>
2021-08-02 18:03:34 -0700

    Build jar with deps.

--- lint/src/main/java/org/datacommons/lint/Lint.java
+++ lint/src/main/java/org/datacommons/lint/Lint.java
@@ -15,9 +15,55 @@ import picocli.CommandLine;
     name = "DCLint",
     mixinStandardHelpOptions = true,
     version = "DCLint 0.1",
-    description = "Tool for use in developing datasets for Data Commons.",
-    subcommands = {Check.class})
-class Lint {
+    description = "Tool for use in developing datasets for Data Commons.")
+class Lint implements Callable<Integer> {
+  private static final Logger logger = LoggerFactory.getLogger(Lint.class);
+
+  @CommandLine.Parameters(
+      arity = "1..*",
+      description =
+          ("List of input files. This depends on the --format option. "
+              + "For 'tmcf', the files are expected to be "
+              + "Template MCF files. For 'mcf', the files are expected to be Instance MCF "
+              + "files. For tmcfCsv, the first file should be a Template MCF file, followed "
+              + "by one or more CSVs compatible with the provided TMCF file."))
+  private File[] files;
+
+  enum FormatType {
+    mcf,
+    tmcf,
+    tmcfCsv
+  };
+
+  @CommandLine.Option(
+      names = {"-f", "--format"},
+      description = "Format of input files: ${COMPLETION-CANDIDATES}",
+      required = true)
+  private FormatType formatType;
+
+  enum CommandType {
+    chk,
+    gen
+  };
+
+  @CommandLine.Option(
+      names = {"-c", "--cmd"},
+      description =
+          "The command to run: ${COMPLETION-CANDIDATES}.  'chk' runs "
+              + "syntax checks on the input files.  'gen' produces instance MCF output, "
+              + "and only makes sense when --format is 'tmcfCsv'. Default: 'chk'",
+      defaultValue = "chk",
+      required = true)
+  private CommandType cmdType;
+
+  @CommandLine.Option(
+      names = {"-d", "--delimiter"},
+      defaultValue = ",",
+      description =
+          "Delimiter of the input CSV file. Relevant only when --format is 'tmcfCsv'. "
+              + "Default: ',' (comma)")
+  private char delimiter;
+
   @CommandLine.Option(
       names = {"-o", "--outputDir"},
       description = "Directory to write output files. Default: /tmp",
@@ -25,61 +71,25 @@ class Lint {
       scope = CommandLine.ScopeType.INHERIT)
   private File outputDir;
 
-  public static void main(String... args) {
-    int exitCode = new CommandLine(new Lint()).execute(args);
-    System.exit(exitCode);
-  }
-}
-
-@CommandLine.Command(
-    name = "check",
-    mixinStandardHelpOptions = true,
-    description = "Check syntax of input files")
-class Check implements Callable<Integer> {
-  private static final Logger logger = LoggerFactory.getLogger(Check.class);
-
-  @CommandLine.Parameters(
-      arity = "1..*",
-      description =
-          ("List of input files. This depends on --type option. "
-              + "For 'tmcf', the files are expected to be "
-              + "Template MCF files. For 'mcf', the files are expected to be Instance MCF "
-              + "files. For tmcfCsv, the first file should be a Template MCF file, followed "
-              + "by one or more CSVs compatible with the provided TMCF file."))
-  private File[] files;
-
-  enum InputType {
-    mcf,
-    tmcf,
-    tmcfCsv
-  };
-
-  @CommandLine.Option(
-      names = {"-t", "--type"},
-      description = "Type of input files: ${COMPLETION-CANDIDATES}",
-      required = true)
-  private InputType type;
-
   @CommandLine.Option(
       names = {"-r", "--isResolved"},
       defaultValue = "false",
       description =
-          "Indicates whether the input file is resolved. Relevant only when " + "--type is 'mcf'")
+          "Indicates whether the input file is resolved. Relevant only when "
+              + "--format is "
+              + "'mcf'. Default: false")
   private boolean isResolved;
 
-  @CommandLine.Option(
-      names = {"-d", "--delimiter"},
-      defaultValue = ",",
-      description =
-          "Delimiter of the input CSV file. Relevant only when --type is 'tmcfCsv'. "
-              + "Defaults to ',' (comma)")
-  private char delimiter;
+  public static void main(String... args) {
+    int exitCode = new CommandLine(new Lint()).execute(args);
+    System.exit(exitCode);
+  }
 
   @Override
   public Integer call() throws IOException {
     Debug.Log.Builder logCtx = Debug.Log.newBuilder();
-    if (type == InputType.tmcfCsv) {
-      if (files.length <= 2) {
+    if (formatType == FormatType.tmcfCsv) {
+      if (files.length < 2) {
         System.err.println("Require two files for option 'tmcfcsv'");
         return -1;
       }
@@ -103,8 +113,8 @@ class Check implements Callable<Integer> {
     } else {
       // MCF or TMCF
       Mcf.McfType mcf_type =
-          (type == InputType.mcf ? Mcf.McfType.INSTANCE_MCF : Mcf.McfType.TEMPLATE_MCF);
-      boolean resolved = (type == InputType.mcf ? isResolved : false);
+          (formatType == FormatType.mcf ? Mcf.McfType.INSTANCE_MCF : Mcf.McfType.TEMPLATE_MCF);
+      boolean resolved = (formatType == FormatType.mcf ? isResolved : false);
       for (File file : files) {
         int numNodesProcessed = 0;
         logger.info("Processing {}", file.getPath());
