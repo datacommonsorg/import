518dc90c9b4ba1690a9a9daeb266a27e01af411e
Prashanth Radhakrishnan <shanth@google.com>
2021-08-02 18:45:15 -0700

    Build jar with deps.

--- lint/src/main/java/org/datacommons/lint/Lint.java
+++ lint/src/main/java/org/datacommons/lint/Lint.java
@@ -1,11 +1,16 @@
 package org.datacommons.lint;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 import org.datacommons.proto.Debug;
 import org.datacommons.proto.Mcf;
 import org.datacommons.util.McfParser;
+import org.datacommons.util.McfUtil;
 import org.datacommons.util.TmcfCsvParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,14 +48,14 @@ class Lint implements Callable<Integer> {
 
   enum CommandType {
     chk,
-    gen
+    genmcf
   };
 
   @CommandLine.Option(
       names = {"-c", "--cmd"},
       description =
           "The command to run: ${COMPLETION-CANDIDATES}.  'chk' runs "
-              + "syntax checks on the input files.  'gen' produces instance MCF output, "
+              + "syntax checks on the input files.  'genmcf' produces instance MCF output, "
               + "and only makes sense when --format is 'tmcfCsv'. Default: 'chk'",
       defaultValue = "chk",
       required = true)
@@ -87,44 +92,69 @@ class Lint implements Callable<Integer> {
 
   @Override
   public Integer call() throws IOException {
-    Debug.Log.Builder logCtx = Debug.Log.newBuilder();
     if (formatType == FormatType.tmcfCsv) {
       if (files.length < 2) {
-        System.err.println("Require two files for option 'tmcfcsv'");
+        System.err.println("At least two input files required with option 'tmcfCsv'.");
         return -1;
       }
-      logger.info("TMCF " + files[0].getPath());
-      for (int i = 1; i < files.length; i++) {
-        logger.info("Processing CSV " + files[i].getPath());
-        TmcfCsvParser parser =
-            TmcfCsvParser.init(files[0].getPath(), files[i].getPath(), delimiter, logCtx);
-        Mcf.McfGraph g;
-        int numNodesProcessed = 0, numRowsProcessed = 0;
-        while ((g = parser.parseNextRow()) != null) {
-          numRowsProcessed++;
-          numNodesProcessed += g.getNodesCount();
-        }
-        logger.info(
-            "Processed CSV {} ({} rows, {} nodes)",
-            files[i].getPath(),
-            numRowsProcessed,
-            numNodesProcessed);
-      }
+      return HandleTableFormat();
     } else {
-      // MCF or TMCF
-      Mcf.McfType mcf_type =
-          (formatType == FormatType.mcf ? Mcf.McfType.INSTANCE_MCF : Mcf.McfType.TEMPLATE_MCF);
-      boolean resolved = (formatType == FormatType.mcf ? isResolved : false);
-      for (File file : files) {
-        int numNodesProcessed = 0;
-        logger.info("Processing {}", file.getPath());
-        McfParser parser = McfParser.init(mcf_type, file.getPath(), resolved);
-        Mcf.McfGraph n;
-        while ((n = parser.parseNextNode()) != null) {
-          numNodesProcessed++;
-        }
-        logger.info("Processed {} ({} nodes)", file.getPath(), numNodesProcessed);
+      if (cmdType == CommandType.genmcf) {
+        System.err.println("Command 'genmcf' is only applicable with --format 'tmcfCsv'");
+        return -1;
       }
+      return HandleNodeFormat();
+    }
+  }
+
+  private Integer HandleNodeFormat() throws IOException {
+    Mcf.McfType mcf_type =
+        (formatType == FormatType.mcf ? Mcf.McfType.INSTANCE_MCF : Mcf.McfType.TEMPLATE_MCF);
+    boolean resolved = (formatType == FormatType.mcf ? isResolved : false);
+    for (File file : files) {
+      int numNodesProcessed = 0;
+      logger.info("Processing {}", file.getPath());
+      McfParser parser = McfParser.init(mcf_type, file.getPath(), resolved);
+      Mcf.McfGraph n;
+      while ((n = parser.parseNextNode()) != null) {
+        numNodesProcessed++;
+      }
+      logger.info("Processed {} ({} nodes)", file.getPath(), numNodesProcessed);
+    }
+    return 0;
+  }
+
+  private Integer HandleTableFormat() throws IOException {
+    Debug.Log.Builder logCtx = Debug.Log.newBuilder();
+    BufferedWriter writer = null;
+    if (cmdType == CommandType.genmcf) {
+      Path outfile = Paths.get(outputDir.getPath(), "generated.mcf");
+      logger.info("Writing to file {}", outfile.toString());
+      writer = new BufferedWriter(new FileWriter(outfile.toString()));
+    }
+    logger.info("TMCF " + files[0].getPath());
+    for (int i = 1; i < files.length; i++) {
+      logger.info("Processing CSV " + files[i].getPath());
+      TmcfCsvParser parser =
+          TmcfCsvParser.init(files[0].getPath(), files[i].getPath(), delimiter, logCtx);
+      Mcf.McfGraph g;
+      int numNodesProcessed = 0, numRowsProcessed = 0;
+      while ((g = parser.parseNextRow()) != null) {
+        numNodesProcessed += g.getNodesCount();
+        numRowsProcessed++;
+        if (writer != null) {
+          writer.write(McfUtil.serializeMcfGraph(g, false));
+        }
+      }
+      logger.info(
+          "Processed CSV {} ({} rows, {} nodes)",
+          files[i].getPath(),
+          numRowsProcessed,
+          numNodesProcessed);
+    }
+    if (writer != null) {
+      writer.close();
+      writer = null;
     }
     return 0;
   }
