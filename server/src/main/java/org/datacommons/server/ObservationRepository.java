e362e50bbf4d209c78458ccd5a7533eff184b279
Bo Xu <boxu@google.com>
2021-08-25 11:27:19 -0700

    lint

--- server/src/main/java/org/datacommons/server/ObservationRepository.java
+++ server/src/main/java/org/datacommons/server/ObservationRepository.java
@@ -24,7 +24,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ObservationRepository extends JpaRepository<Observation, Long> {
   @Query(
-      value = "SELECT * FROM OBSERVATION o WHERE o.OBSERVATION_ABOUT = ?1 and o.VARIABLE_MEASURED = ?2",
+      value =
+          "SELECT * FROM OBSERVATION o WHERE o.OBSERVATION_ABOUT = ?1 and o.VARIABLE_MEASURED = ?2",
       nativeQuery = true)
   List<Observation> findObservationByPlaceAndStatVar(String place, String statVar);
 }
