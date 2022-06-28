14a85ac021b865b0a067cea117599c1ccf80464b
Jehangir Amjad <jehangiramjad@gmail.com>
2022-06-28 15:31:26 -0700

    Several fixes post comments

--- wizard/src/utils/detect_place.ts
+++ wizard/src/utils/detect_place.ts
@@ -16,23 +16,26 @@
 import { DetectedDetails, DetectedFormat, ConfidenceLevel } from "../types";
 import countriesJSON from "./country_mappings.json";
 
-// A PlaceDetector objected is meant to be initialized once. It provides
-// convenience access to all place types and their supported formats. It also
-// supports detecting the place type for each individual column (a header and
-// a list of string values).
+/**
+* A PlaceDetector objected is meant to be initialized once. It provides
+* convenience access to all place types and their supported formats. It also
+* supports detecting the place type for each individual column (a header and
+* a list of string values).
+*/
 export class PlaceDetector {
   countryNames: Set<string>;
   countryISO: Set<string>;
   countryAbbrv3: Set<string>;
   countryNumeric: Set<string>;
 
-  placeTypes: Set<string>;
+  placeTypes: Map<string, string>;
 
   static typeFormatMappings = new Map<string, Array<DetectedFormat>>(
     [
-      ["Longitude",   [ {propertyName: "name", displayName: "Full Name"},]],
-      ["Latitude",    [ {propertyName: "name", displayName: "Full Name"},]],
-      ["LatLon",      [ {propertyName: "name", displayName: "Full Name"},]],
+      ["Longitude",   [ {propertyName: "longitude", displayName: "Longitude"},]],
+      ["Latitude",    [ {propertyName: "latitude", displayName: "Latitude"},]],
+      ["LatLon",      [ {propertyName: "GeoCoordinates", displayName: "Geo Coordinates"},]],
+      ["GeoCoordinates",[ {propertyName: "GeoCoordinates", displayName: "Geo Coordinates"},]],
       ["Country",     [ {propertyName: "name", displayName: "Full Name"},
                         {propertyName: "isoCode", displayName: "ISO Code"},
                         {propertyName: "countryAlpha3Code", displayName: "Alpha 3 Code"},
@@ -49,18 +52,24 @@ export class PlaceDetector {
     // Set the various class attributes.
     this.preProcessCountries();
 
-    this.placeTypes = new Set<string>();
-    for (var key of Array.from(PlaceDetector.typeFormatMappings.keys())) {
-      this.placeTypes.add(key.toLowerCase());
+    this.placeTypes = new Map<string, string>();
+    for (let key of Array.from(PlaceDetector.typeFormatMappings.keys())) {
+      this.placeTypes.set(key.toLowerCase(), key);
     }
   }
 
-  // Returns a Map of all place types and their supported formats.
+  /**
+  * Returns a Map of all place types and their supported formats.
+  *
+  * @return a map of place types (string) to the supported formats (array).
+  */
   validPlaceTypesAndFormats(): Map<string, Array<DetectedFormat>> {
     return PlaceDetector.typeFormatMappings;
   }
 
-  // Process the countriesJSON object to generate the required sets.
+  /**
+  * Process the countriesJSON object to generate the required sets.
+  */
   preProcessCountries() {
     this.countryNames = new Set<string>();
     this.countryISO = new Set<string>();
@@ -82,36 +91,39 @@ export class PlaceDetector {
     }
   }
 
-  // The low confidence column detector simply checks if the column header
-  // (string) matches one of the supported place strings in this.placeTypes.
-  // The header is converted to lower case and only alphanumeric chars are used.
-  // If there is no match, the return value is null.
+  /**
+  * The low confidence column detector simply checks if the column header
+  * (string) matches one of the supported place strings in this.placeTypes.
+  * The header is converted to lower case and only alphanumeric chars are used.
+  * If there is no match, the return value is null.
+  *
+  * @param header the name of the column.
+  *
+  * @return the place type string (or null).
+  */
   detectLowConfidence(header: string): string {
-    var h = header.toLowerCase().replace(/[^a-z0-9]/gi,'')
-
-    if (this.placeTypes.has(h)) {
-      var place = h.charAt(0).toUpperCase() + h.substr(1);
-
-      // "LatLon" needs special care after the processing above.
-      if (place == "Latlon") {
-        return "LatLon";
-      }
-      return place;
-    }
-    return null;
+    const h = header.toLowerCase().replace(/[^a-z0-9]/gi,'');
+    return this.placeTypes.has(h) ? this.placeTypes.get(h) : null;
   }
 
-  // Detecting Place.
-  // header: the column header string.
-  // column: an array of string values.
-  // If nothing is detected, null is returned.
-  // Otherwise, the detectedType, the detectedFormat and and confidence level
-  // are returned.
+  /**
+  * Detecting Place.
+  * If nothing is detected, null is returned.
+  * Otherwise, the detectedType, the detectedFormat and and confidence level
+  * are returned.
+  * It is up to the consumer, e.g. in heuristics.ts, to decide whether to
+  * pass the low confidence detection back to the user (or not).
+  *
+  * @param header: the column header string.
+  * @param column: an array of string column values.
+  *
+  * @return the DetectedDetails object (or null).
+  */
   detect(header: string, column: Array<string>): DetectedDetails {
 
     // High Confidence detection is TBD. For now, only doing Low Confidence
     // detection.
-    var lcDetected = this.detectLowConfidence(header);
+    const lcDetected = this.detectLowConfidence(header);
     if (lcDetected == null) {
       return null;
     }
