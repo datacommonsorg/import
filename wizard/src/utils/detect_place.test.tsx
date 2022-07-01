2d87273eeabc90c42dc3b7b0c3ed41d70bd7a5b4
Jehangir Amjad <1021616+jehangiramjad@users.noreply.github.com>
2022-07-01 12:20:45 -0700

    High Confidence country detection (#143)
    * Initial Location detection implementation.
    
    * updates based on comments
    
    * renaming location to place
    
    * addressing comments
    
    * defining the CsvData interface
    
    * change the keys to be of type number
    
    * updating key to BigInt
    
    * updating the comment
    
    * Low Conf Place Detection
    
    * high conf place detection
    
    * Several fixes post comments
    
    * more abstractions after discussion with @shanth
    
    * updating comment to explain keyTypePropertyMappings better
    
    * renaming
    
    * high conf country detection
    
    * addressing comments
    
    * fixing property name casing
    
    * resolving merge conflicts

--- wizard/src/utils/detect_place.test.tsx
+++ wizard/src/utils/detect_place.test.tsx
@@ -34,39 +34,39 @@ test("placeTypesAndProperties", () => {
       dcProperty: { dcid: "name", displayName: "Name" },
     },
     {
-      dcType: { dcid: "country", displayName: "Country" },
+      dcType: { dcid: "Country", displayName: "Country" },
       dcProperty: { dcid: "name", displayName: "Name" },
     },
     {
-      dcType: { dcid: "country", displayName: "Country" },
+      dcType: { dcid: "Country", displayName: "Country" },
       dcProperty: { dcid: "isoCode", displayName: "ISO Code" },
     },
     {
-      dcType: { dcid: "country", displayName: "Country" },
+      dcType: { dcid: "Country", displayName: "Country" },
       dcProperty: { dcid: "countryAlpha3Code", displayName: "Alpha 3 Code" },
     },
     {
-      dcType: { dcid: "country", displayName: "Country" },
+      dcType: { dcid: "Country", displayName: "Country" },
       dcProperty: { dcid: "countryNumericCode", displayName: "Numeric Code" },
     },
     {
-      dcType: { dcid: "state", displayName: "State" },
+      dcType: { dcid: "State", displayName: "State" },
       dcProperty: { dcid: "name", displayName: "Name" },
     },
     {
-      dcType: { dcid: "province", displayName: "Province" },
+      dcType: { dcid: "Province", displayName: "Province" },
       dcProperty: { dcid: "name", displayName: "Name" },
     },
     {
-      dcType: { dcid: "municipality", displayName: "Municipality" },
+      dcType: { dcid: "Municipality", displayName: "Municipality" },
       dcProperty: { dcid: "name", displayName: "Name" },
     },
     {
-      dcType: { dcid: "county", displayName: "County" },
+      dcType: { dcid: "County", displayName: "County" },
       dcProperty: { dcid: "name", displayName: "Name" },
     },
     {
-      dcType: { dcid: "city", displayName: "City" },
+      dcType: { dcid: "City", displayName: "City" },
       dcProperty: { dcid: "name", displayName: "Name" },
     },
   ]);
@@ -90,6 +90,41 @@ test("placeDetectionKeys", () => {
   expect(got).toEqual(expected);
 });
 
+test("countries", () => {
+  const det = new PlaceDetector();
+  const numCountriesExpected = 271;
+  const numIsoCodes = 247; // 24 countries without ISO codes.
+  const numAbbrv3Codes = 246; // 25 countries without 3-letter abbreviations.
+  const numNumeric = 246; // 25 countries without numeric codes.
+  expect(det.countryNames.size).toEqual(numCountriesExpected);
+  expect(det.countryISO.size).toEqual(numIsoCodes);
+  expect(det.countryAbbrv3.size).toEqual(numAbbrv3Codes);
+  expect(det.countryNumeric.size).toEqual(numNumeric); // Some other random spot checks. // Norway.
+
+  expect(det.countryNames).toContain("norway");
+  expect(det.countryISO).toContain("no");
+  expect(det.countryAbbrv3).toContain("nor");
+  expect(det.countryNumeric).toContain("578"); // Senegal.
+
+  expect(det.countryNames).toContain("senegal");
+  expect(det.countryISO).toContain("sn");
+  expect(det.countryAbbrv3).toContain("sen");
+  expect(det.countryNumeric).toContain("686"); // Other checks.
+
+  expect(det.countryNames).not.toContain("");
+  expect(det.countryNames).not.toContain(null);
+
+  expect(det.countryISO).not.toContain("");
+  expect(det.countryISO).not.toContain(null);
+
+  expect(det.countryAbbrv3).not.toContain("");
+  expect(det.countryAbbrv3).not.toContain(null);
+
+  expect(det.countryNumeric).not.toContain("");
+  expect(det.countryNumeric).not.toContain(null);
+  expect(det.countryNumeric).not.toContain(0);
+});
+
 test("placeLowConfidenceDetection", () => {
   const det = new PlaceDetector();
 
@@ -97,10 +132,10 @@ test("placeLowConfidenceDetection", () => {
   expect(det.detectLowConfidence(" ")).toBe(null);
   expect(det.detectLowConfidence("continent")).toBe(null);
 
-  const countryType = { dcType: { dcid: "country", displayName: "Country" } };
-  const stateType = { dcType: { dcid: "state", displayName: "State" } };
-  const countyType = { dcType: { dcid: "county", displayName: "County" } };
-  const cityType = { dcType: { dcid: "city", displayName: "City" } };
+  const countryType = { dcType: { dcid: "Country", displayName: "Country" } };
+  const stateType = { dcType: { dcid: "State", displayName: "State" } };
+  const countyType = { dcType: { dcid: "County", displayName: "County" } };
+  const cityType = { dcType: { dcid: "City", displayName: "City" } };
 
   expect(_.isEqual(det.detectLowConfidence("Country.."), countryType)).toBe(
     true
@@ -144,16 +179,203 @@ test("detectionLowConf", () => {
   expect(det.detect("", [])).toBe(null);
 
   const expected: DetectedDetails = {
-    detectedTypeProperty: { dcType: { dcid: "city", displayName: "City" } },
+    detectedTypeProperty: { dcType: { dcid: "City", displayName: "City" } },
     confidence: ConfidenceLevel.Low,
   };
   const notExpected: DetectedDetails = {
-    detectedTypeProperty: { dcType: { dcid: "city", displayName: "City" } },
+    detectedTypeProperty: { dcType: { dcid: "City", displayName: "City" } },
     confidence: ConfidenceLevel.High, // should be Low.
   };
 
+  // Should return low confidence detection of a city.
   const got = det.detect("city", ["a", "b", "c"]);
 
   expect(_.isEqual(expected, got)).toBe(true);
   expect(_.isEqual(notExpected, got)).toBe(false);
+});
+
+test("countryHighConf", () => {
+  const det = new PlaceDetector();
+  const cases: {
+    name: string;
+    colArray: Array<string>;
+    expected: TypeProperty;
+  }[] = [
+    {
+      name: "name-detection",
+      colArray: [
+        "United states",
+        "Norway",
+        "sri lanka",
+        "new zealand",
+        "south africa",
+        "australia",
+        "Pakistan",
+        "India",
+        "bangladesh",
+        "french Afars and Issas",
+      ],
+      expected: {
+        dcType: { dcid: "Country", displayName: "Country" },
+        dcProperty: { dcid: "name", displayName: "Name" },
+      },
+    },
+    {
+      name: "iso-detection",
+      colArray: ["us", "no", "lk", "nz", "sa", "au", "pk", "in", "bd", "it"],
+      expected: {
+        dcType: { dcid: "Country", displayName: "Country" },
+        dcProperty: { dcid: "isoCode", displayName: "ISO Code" },
+      },
+    },
+    {
+      name: "alpha3-detection",
+      colArray: [
+        "usa",
+        "NOR",
+        "lka",
+        "nzl",
+        "zaf",
+        "aus",
+        "pak",
+        "ind",
+        "bgd",
+        "ita",
+      ],
+      expected: {
+        dcType: { dcid: "Country", displayName: "Country" },
+        dcProperty: {
+          dcid: "countryAlpha3Code",
+          displayName: "Alpha 3 Code",
+        },
+      },
+    },
+    {
+      name: "numeric-detection",
+      colArray: [
+        "840",
+        "578",
+        "144",
+        "554",
+        "710",
+        "36",
+        "586",
+        "356",
+        "50",
+        "380",
+      ],
+      expected: {
+        dcType: { dcid: "Country", displayName: "Country" },
+        dcProperty: {
+          dcid: "countryNumericCode",
+          displayName: "Numeric Code",
+        },
+      },
+    },
+    {
+      name: "numeric-detection-with-null",
+      colArray: [
+        "840",
+        "578",
+        "144",
+        "554",
+        "710",
+        "36",
+        "586",
+        null,
+        null,
+        null,
+      ],
+      expected: {
+        dcType: { dcid: "Country", displayName: "Country" },
+        dcProperty: {
+          dcid: "countryNumericCode",
+          displayName: "Numeric Code",
+        },
+      },
+    },
+    {
+      name: "name-no-detection", //No detection due to: united statesssss, norwayyyy and north africa.
+      colArray: [
+        "United statesssss",
+        "Norwayyyy",
+        "sri lanka",
+        "new zealand",
+        "north africa",
+        "australia",
+        "Pakistan",
+        "India",
+        "bangladesh",
+        "french Afars and Issas",
+      ],
+      expected: null,
+    },
+    {
+      name: "iso-no-detection", // No detection due to: aa, bb and cc.
+      colArray: ["aa", "bb", "cc", "nz", "za", "au", "pk", "in", "bd", "it"],
+      expected: null,
+    },
+    {
+      name: "alpha3-no-detection", // No detection due to: aaa, bbb and ccc.
+      colArray: [
+        "aaa",
+        "bbb",
+        "ccc",
+        "nzl",
+        "zaf",
+        "aus",
+        "pak",
+        "ind",
+        "bgd",
+        "ita",
+      ],
+      expected: null,
+    },
+    {
+      name: "numeric-no-detection", // No detection due to: 0, 1, 2.
+      colArray: ["0", "1", "2", "554", "710", "36", "586", "356", "50", "380"],
+      expected: null,
+    },
+  ];
+  for (const c of cases) {
+    if (c.expected == null) {
+      expect(det.detect("", c.colArray)).toBe(null);
+      continue;
+    }
+    expect(det.detect("", c.colArray)).toStrictEqual({
+      detectedTypeProperty: c.expected,
+      confidence: ConfidenceLevel.High,
+    });
+  }
+});
+
+test("placeDetection", () => {
+  const det = new PlaceDetector();
+  const colName = "country";
+
+  // High Conf. Detection.
+  const colArray = ["USA", "NOR", "ITA"];
+  const expectedHighConf = {
+    detectedTypeProperty: {
+      dcType: { dcid: "Country", displayName: "Country" },
+      dcProperty: { dcid: "countryAlpha3Code", displayName: "Alpha 3 Code" },
+    },
+    confidence: ConfidenceLevel.High,
+  };
+
+  expect(det.detect(colName, colArray)).toStrictEqual(expectedHighConf);
+
+  // Modifying colArray to ensure country cannot be detected with high conf.
+  // But it should be a low confidence detection due to the colName.
+  colArray[0] = "randomString";
+  const expectedLowConf = {
+    detectedTypeProperty: {
+      dcType: { dcid: "Country", displayName: "Country" },
+    },
+    confidence: ConfidenceLevel.Low,
+  };
+  expect(det.detect(colName, colArray)).toStrictEqual(expectedLowConf);
+
+  // Now, use a column name which is not found to result in no detection.
+  expect(det.detect("", colArray)).toStrictEqual(null);
 });
