81572a209613e0c0f70167b8644d67305d6a1249
Keyur Shah <keyurs@google.com>
2023-09-15 12:49:16 -0700

    Address review comments.

--- util/src/main/java/org/datacommons/util/NameResolver.java
+++ util/src/main/java/org/datacommons/util/NameResolver.java
@@ -16,6 +16,8 @@ import org.datacommons.proto.Resolve.ResolveResponse.Entity.Candidate;
 
 /** Resolves nodes with names by calling the DC resolution API. */
 final class NameResolver {
+  // Property in the resolve request.
+  private static final String DESCRIPTION_DCID = "<-description->dcid";
   // Names to be resolved.
   private final Set<String> resolveNames = ConcurrentHashMap.newKeySet();
 
@@ -40,16 +42,21 @@ final class NameResolver {
 
   void drain() {
     if (!resolveNames.isEmpty()) {
+      // TODO: Support resolving based on type.
+      // See:
+      // https://github.com/datacommonsorg/mixer/blob/master/internal/server/v2/resolve/golden/resolve_description_test.go#L50
       ResolveRequest request =
           ResolveRequest.newBuilder()
               .addAllNodes(resolveNames)
-              .setProperty("<-description->dcid")
+              .setProperty(DESCRIPTION_DCID)
               .build();
       ResolveResponse response = client.resolve(request);
       populateResolvedNames(response);
     }
   }
 
+  // TODO: Instead of returning the first one always, allow client to optionally set the type of
+  // candidate to return.
   Optional<String> resolve(PropertyValues node) {
     return getName(node)
         .filter(resolvedNames::containsKey)
