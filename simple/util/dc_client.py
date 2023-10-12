f9708d96ee31c1222a7d5549d45d74b28061823e
Bo Xu <shifucun@users.noreply.github.com>
2023-10-12 11:07:57 -0700

    trigger

--- simple/util/dc_client.py
+++ simple/util/dc_client.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Data Commons REST API Client.
-"""
+""" Data Commons REST API Client."""
 
 import json
 import logging
