02ab6bafa081d37e77374d75b69d5c93ed6cc810
Bo Xu <shifucun@users.noreply.github.com>
2023-10-12 18:09:46 +0000

    Added python lint locally and in cloudbuild (#229)
    * Added python lint locally and in cloudbuild
    
    * trigger

--- simple/util/filehandler.py
+++ simple/util/filehandler.py
@@ -15,139 +15,138 @@
 local and GCS files and directories.
 """
 
-import logging
 import io
+import logging
 import os
+
 from google.cloud import storage
 
 _GCS_PATH_PREFIX = "gs://"
 
 
 class FileHandler:
-    """(Abstract) base class that should be extended by concrete implementations."""
+  """(Abstract) base class that should be extended by concrete implementations."""
 
-    def __init__(self, path: str, isdir: bool) -> None:
-        self.path = path
-        self.isdir = isdir
+  def __init__(self, path: str, isdir: bool) -> None:
+    self.path = path
+    self.isdir = isdir
 
-    def __str__(self) -> str:
-        return self.path
+  def __str__(self) -> str:
+    return self.path
 
-    def read_string(self) -> str:
-        pass
+  def read_string(self) -> str:
+    pass
 
-    def read_string_io(self) -> io.StringIO:
-        return io.StringIO(self.read_string())
+  def read_string_io(self) -> io.StringIO:
+    return io.StringIO(self.read_string())
 
-    def write_string(self, content: str) -> None:
-        pass
+  def write_string(self, content: str) -> None:
+    pass
 
-    def make_file(self, file_name: str) -> "FileHandler":
-        pass
+  def make_file(self, file_name: str) -> "FileHandler":
+    pass
 
-    def make_dirs(self) -> None:
-        pass
+  def make_dirs(self) -> None:
+    pass
 
-    def basename(self) -> str:
-        pass
+  def basename(self) -> str:
+    pass
 
-    def exists(self) -> bool:
-        pass
+  def exists(self) -> bool:
+    pass
 
-    def list_files(self, extension: str = None) -> list[str]:
-        pass
+  def list_files(self, extension: str = None) -> list[str]:
+    pass
 
 
 class LocalFileHandler(FileHandler):
 
-    def __init__(self, path: str) -> None:
-        isdir = os.path.isdir(path)
-        super().__init__(path, isdir)
+  def __init__(self, path: str) -> None:
+    isdir = os.path.isdir(path)
+    super().__init__(path, isdir)
 
-    def read_string(self) -> str:
-        with open(self.path, "r") as f:
-            return f.read()
+  def read_string(self) -> str:
+    with open(self.path, "r") as f:
+      return f.read()
 
-    def write_string(self, content: str) -> None:
-        with open(self.path, "w") as f:
-            f.write(content)
+  def write_string(self, content: str) -> None:
+    with open(self.path, "w") as f:
+      f.write(content)
 
-    def make_file(self, file_name: str) -> FileHandler:
-        return LocalFileHandler(os.path.join(self.path, file_name))
+  def make_file(self, file_name: str) -> FileHandler:
+    return LocalFileHandler(os.path.join(self.path, file_name))
 
-    def make_dirs(self) -> None:
-        return os.makedirs(self.path, exist_ok=True)
+  def make_dirs(self) -> None:
+    return os.makedirs(self.path, exist_ok=True)
 
-    def basename(self) -> str:
-        path = self.path.rstrip(self.path[-1]) if self.path.endswith(
-            os.sep) else self.path
-        return path.split(os.sep)[-1]
+  def basename(self) -> str:
+    path = self.path.rstrip(self.path[-1]) if self.path.endswith(
+        os.sep) else self.path
+    return path.split(os.sep)[-1]
 
-    def exists(self) -> bool:
-        return os.path.exists(self.path)
+  def exists(self) -> bool:
+    return os.path.exists(self.path)
 
-    def list_files(self, extension: str = None) -> list[str]:
-        all_files = os.listdir(self.path)
-        if not extension:
-            return all_files
-        return filter(lambda name: name.lower().endswith(extension.lower()),
-                      all_files)
+  def list_files(self, extension: str = None) -> list[str]:
+    all_files = os.listdir(self.path)
+    if not extension:
+      return all_files
+    return filter(lambda name: name.lower().endswith(extension.lower()),
+                  all_files)
 
 
 class GcsMeta(type):
 
-    @property
-    def gcs_client(cls) -> storage.Client:
-        if getattr(cls, "_GCS_CLIENT", None) is None:
-            gcs_client = storage.Client()
-            logging.info("Using GCS project: %s", gcs_client.project)
-            cls._GCS_CLIENT = gcs_client
-        return cls._GCS_CLIENT
+  @property
+  def gcs_client(cls) -> storage.Client:
+    if getattr(cls, "_GCS_CLIENT", None) is None:
+      gcs_client = storage.Client()
+      logging.info("Using GCS project: %s", gcs_client.project)
+      cls._GCS_CLIENT = gcs_client
+    return cls._GCS_CLIENT
 
 
 class GcsFileHandler(FileHandler, metaclass=GcsMeta):
 
-    def __init__(self, path: str) -> None:
-        if not path.startswith(_GCS_PATH_PREFIX):
-            raise ValueError(f"Expected {_GCS_PATH_PREFIX} prefix, got {path}")
-        bucket_name, blob_name = path[len(_GCS_PATH_PREFIX):].split('/', 1)
-        self.bucket = GcsFileHandler.gcs_client.bucket(bucket_name)
-        self.blob = self.bucket.blob(blob_name)
-        isdir = path.endswith("/")
-        super().__init__(path, isdir)
+  def __init__(self, path: str) -> None:
+    if not path.startswith(_GCS_PATH_PREFIX):
+      raise ValueError(f"Expected {_GCS_PATH_PREFIX} prefix, got {path}")
+    bucket_name, blob_name = path[len(_GCS_PATH_PREFIX):].split('/', 1)
+    self.bucket = GcsFileHandler.gcs_client.bucket(bucket_name)
+    self.blob = self.bucket.blob(blob_name)
+    isdir = path.endswith("/")
+    super().__init__(path, isdir)
 
-    def read_string(self) -> str:
-        return self.blob.download_as_string().decode("utf-8")
+  def read_string(self) -> str:
+    return self.blob.download_as_string().decode("utf-8")
 
-    def write_string(self, content: str) -> None:
-        self.blob.upload_from_string(content)
+  def write_string(self, content: str) -> None:
+    self.blob.upload_from_string(content)
 
-    def make_file(self, file_name: str) -> FileHandler:
-        return GcsFileHandler(
-            f"{self.path}{'' if self.isdir else '/'}{file_name}")
+  def make_file(self, file_name: str) -> FileHandler:
+    return GcsFileHandler(f"{self.path}{'' if self.isdir else '/'}{file_name}")
 
-    def basename(self) -> str:
-        path = self.path.rstrip(
-            self.path[-1]) if self.path.endswith("/") else self.path
-        return path.split("/")[-1]
+  def basename(self) -> str:
+    path = self.path.rstrip(
+        self.path[-1]) if self.path.endswith("/") else self.path
+    return path.split("/")[-1]
 
-    def exists(self) -> bool:
-        return self.blob.exists()
+  def exists(self) -> bool:
+    return self.blob.exists()
 
-    def list_files(self, extension: str = None) -> list[str]:
-        prefix = self.blob.name if self.path.endswith(
-            "/") else f"{self.blob.name}/"
-        all_files = [
-            blob.name[len(prefix):]
-            for blob in self.bucket.list_blobs(prefix=prefix, delimiter="/")
-        ]
-        if not extension:
-            return all_files
-        return filter(lambda name: name.lower().endswith(extension.lower()),
-                      all_files)
+  def list_files(self, extension: str = None) -> list[str]:
+    prefix = self.blob.name if self.path.endswith("/") else f"{self.blob.name}/"
+    all_files = [
+        blob.name[len(prefix):]
+        for blob in self.bucket.list_blobs(prefix=prefix, delimiter="/")
+    ]
+    if not extension:
+      return all_files
+    return filter(lambda name: name.lower().endswith(extension.lower()),
+                  all_files)
 
 
 def create_file_handler(path: str) -> FileHandler:
-    if path.startswith(_GCS_PATH_PREFIX):
-        return GcsFileHandler(path)
-    return LocalFileHandler(path)
+  if path.startswith(_GCS_PATH_PREFIX):
+    return GcsFileHandler(path)
+  return LocalFileHandler(path)
