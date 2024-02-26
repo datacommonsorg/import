c4900342658e2664f92d2691f3a98a635eaf4d4f
Keyur Shah <keyurs@google.com>
2024-02-26 11:36:09 -0800

    Make docker image runnable via a config file.

--- build/simple/run.sh
+++ build/simple/run.sh
@@ -13,24 +13,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [[ $GCS_INPUT_DIR != "" ]]; then
-    export INPUT_DIR=$GCS_INPUT_DIR
-else
-    export INPUT_DIR=/input/
-fi
-
 if [[ $GCS_OUTPUT_DIR != "" ]]; then
     export OUTPUT_DIR=$GCS_OUTPUT_DIR
 else
     export OUTPUT_DIR=/output/
 fi
 
-echo "DC_API_KEY=$DC_API_KEY"
-echo "INPUT_DIR=$INPUT_DIR"
-echo "OUTPUT_DIR=$INPUT_DIR"
+if [[ $CONFIG_FILE != "" ]]; then
+    echo "DC_API_KEY=$DC_API_KEY"
+    echo "CONFIG_FILE=$CONFIG_FILE"
+    echo "OUTPUT_DIR=$OUTPUT_DIR"
 
-python3 -m stats.main \
-    --mode=maindc \
-    --input_dir=$INPUT_DIR \
-    --output_dir=$OUTPUT_DIR
+    python3 -m stats.main \
+        --mode=maindc \
+        --config_file=$CONFIG_FILE \
+        --output_dir=$OUTPUT_DIR
+else
+    if [[ $GCS_INPUT_DIR != "" ]]; then
+        export INPUT_DIR=$GCS_INPUT_DIR
+    else
+        export INPUT_DIR=/input/
+    fi
+
+    echo "DC_API_KEY=$DC_API_KEY"
+    echo "INPUT_DIR=$INPUT_DIR"
+    echo "OUTPUT_DIR=$OUTPUT_DIR"
+
+    python3 -m stats.main \
+        --mode=maindc \
+        --input_dir=$INPUT_DIR \
+        --output_dir=$OUTPUT_DIR
+fi
 
--- tools/simple/README.md
+++ tools/simple/README.md
@@ -3,6 +3,10 @@
 `run_simple.sh` is an example script illustrating how the simple import docker image
 can be run locally.
 
-In the `env.list` file, specify the `DC_API_KEY`.
-If using local input and output dirs, specify `INPUT_DIR` and `OUTPUT_DIR` respectively.
-If using GCS input and output dirs, specify `GCS_INPUT_DIR` and `GCS_OUTPUT_DIR` respectively.
+* In the `env.list` file, specify the `DC_API_KEY`.
+* If using GCS:
+  * Run `gcloud beta auth application-default login`.
+  * Specify your cloud project via the `GOOGLE_CLOUD_PROJECT` variable.
+* If using config file and output dirs, specify `CONFIG_FILE` and `GCS_OUTPUT_DIR` (or `OUTPUT_DIR`) respectively.
+* If using local input and output dirs, specify `INPUT_DIR` and `OUTPUT_DIR` respectively.
+* If using GCS input and output dirs, specify `GCS_INPUT_DIR` and `GCS_OUTPUT_DIR` respectively.
