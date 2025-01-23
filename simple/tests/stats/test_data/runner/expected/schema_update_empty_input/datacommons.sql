c6b39066a52720ed3b4c55554aedc0f8775efdc4
Hannah Pho <hpho@google.com>
2025-01-23 15:57:46 -0500

    Support schema update mode with totally empty input

---
+++ simple/tests/stats/test_data/runner/expected/schema_update_empty_input/datacommons.sql
@@ -0,0 +1,31 @@
+BEGIN TRANSACTION;
+CREATE TABLE imports (
+    imported_at datetime,
+    status varchar(16),
+    metadata text
+);
+CREATE TABLE key_value_store (
+    lookup_key varchar(255),
+    value longtext
+);
+CREATE TABLE observations (
+    entity varchar(255),
+    variable varchar(255),
+    date varchar(255),
+    value varchar(255),
+    provenance varchar(255),
+    unit varchar(255),
+    scaling_factor varchar(255),
+    measurement_method varchar(255),
+    observation_period varchar(255),
+    properties TEXT
+);
+CREATE TABLE triples (
+    subject_id varchar(255),
+    predicate varchar(255),
+    object_id varchar(255),
+    object_value TEXT
+);
+CREATE INDEX observations_entity_variable on observations (entity, variable);
+CREATE INDEX triples_subject_id on triples (subject_id);
+COMMIT;
