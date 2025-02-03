c5b8d9bce68e7118949ccdd7d888328cdc4f26a8
Hannah Pho <hpho@google.com>
2025-02-03 14:55:34 -0500

    Define DbCache and RedisDbCache

---
+++ simple/stats/db_cache.py
@@ -0,0 +1,44 @@
+import os
+
+import redis
+
+ENV_REDIS_HOST = "REDIS_HOST"
+ENV_REDIS_PORT = "REDIS_PORT"
+DEFAULT_REDIS_PORT = 6739
+
+
+class DbCache:
+  """Abstract class for a database cache layer.
+
+  Provides only the interface needed by the data loading process.
+  """
+
+  def clear(self):
+    pass
+
+
+class RedisDbCache(DbCache):
+  """Connects to a Redis instance used as a database cache and provides methods to interact with it."""
+
+  def __init__(self, host: str, port: int):
+    self.connection = redis.Redis(host=host, port=port, decode_responses=True)
+    self.connection.ping()
+
+  def clear(self):
+    """Flushes all data from the Redis instance used as a database cache."""
+    self.connection.flushall(asynchronous=False)
+
+
+def get_db_cache_from_env() -> DbCache | None:
+  """Returns an appropriate database cache interface based on environment variables."""
+  redis_host = os.getenv(ENV_REDIS_HOST)
+  if not redis_host:
+    return None
+  redis_port = DEFAULT_REDIS_PORT
+  redis_port_str = os.getenv(ENV_REDIS_PORT)
+  if redis_port_str:
+    try:
+      redis_port = int(redis_port_str)
+    except ValueError:
+      raise ValueError(f"Invalid REDIS_PORT: {redis_port_str}")
+  return RedisDbCache(host=redis_host, port=redis_port)
---
+++ simple/tests/stats/db_cache_test.py
@@ -0,0 +1,67 @@
+import os
+import unittest
+from unittest.mock import patch
+
+from fakeredis import FakeRedis
+from fakeredis import FakeServer
+from redis.exceptions import ConnectionError
+from stats.db_cache import DEFAULT_REDIS_PORT
+from stats.db_cache import ENV_REDIS_HOST
+from stats.db_cache import ENV_REDIS_PORT
+from stats.db_cache import get_db_cache_from_env
+from stats.db_cache import RedisDbCache
+
+
+class TestDbCache(unittest.TestCase):
+
+  @patch.dict(os.environ, {ENV_REDIS_HOST: "localhost"})
+  def test_db_cache_redis_default_port(self):
+    with patch("redis.Redis", return_value=FakeRedis()) as redis_constructor:
+      cache = get_db_cache_from_env()
+      self.assertIsInstance(cache, RedisDbCache)
+      redis_constructor.assert_called_with(host="localhost",
+                                           port=DEFAULT_REDIS_PORT,
+                                           decode_responses=True)
+
+  @patch.dict(os.environ, {ENV_REDIS_HOST: "localhost", ENV_REDIS_PORT: "6740"})
+  def test_db_cache_redis_custom_port(self):
+    with patch("redis.Redis", return_value=FakeRedis()) as redis_constructor:
+      cache = get_db_cache_from_env()
+      self.assertIsInstance(cache, RedisDbCache)
+      redis_constructor.assert_called_with(host="localhost",
+                                           port=6740,
+                                           decode_responses=True)
+
+  @patch.dict(os.environ, {
+      ENV_REDIS_HOST: "localhost",
+      ENV_REDIS_PORT: "invalid"
+  })
+  def test_db_cache_redis_invalid_port(self):
+    with patch("redis.Redis"):  # Mock redis.Redis to avoid actual connection
+      with self.assertRaises(ValueError):
+        get_db_cache_from_env()
+
+  @patch.dict(os.environ, {ENV_REDIS_PORT: "unused"})
+  def test_get_db_cache_redis_no_host(self):
+    with patch("redis.Redis"):  # Mock redis.Redis to avoid actual connection
+      cache = get_db_cache_from_env()
+      self.assertIsNone(cache)
+
+  @patch.dict(os.environ, {ENV_REDIS_HOST: "localhost"})
+  def test_get_db_cache_redis_no_connection(self):
+    server = FakeServer()
+    server.connected = False
+    fake_redis = FakeRedis(server=server)
+    with patch("redis.Redis", return_value=fake_redis):
+      self.assertRaises(ConnectionError, get_db_cache_from_env)
+
+  @patch.dict(os.environ, {ENV_REDIS_HOST: "localhost"})
+  def test_get_db_cache_redis_clear(self):
+    fake_redis = FakeRedis()
+    fake_redis.set("somekey", "somevalue")
+    self.assertEquals(1, len(fake_redis.keys("*")))
+    with patch("redis.Redis", return_value=fake_redis):
+      cache = get_db_cache_from_env()
+      self.assertIsInstance(cache, RedisDbCache)
+      cache.clear()
+      self.assertEquals(0, len(fake_redis.keys("*")))
