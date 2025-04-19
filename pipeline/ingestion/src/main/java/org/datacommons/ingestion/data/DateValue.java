050b15a80d35c2b09aea04cc0bfabbd7721a7ae5
Keyur Shah <keyurs@google.com>
2025-04-19 16:00:01 -0700

    Create pipeline for ingesting observations.

--- pipeline/ingestion/src/main/java/org/datacommons/ingestion/data/DateValue.java
+++ pipeline/ingestion/src/main/java/org/datacommons/ingestion/data/DateValue.java
@@ -1,6 +1,9 @@
 package org.datacommons.ingestion.data;
 
+import com.google.gson.Gson;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import org.apache.beam.sdk.coders.DefaultCoder;
 import org.apache.beam.sdk.extensions.avro.coders.AvroCoder;
@@ -8,8 +11,10 @@ import org.apache.beam.sdk.extensions.avro.coders.AvroCoder;
 /** Models an observation value at a specific date. */
 @DefaultCoder(AvroCoder.class)
 public class DateValue implements Serializable {
-  private String date;
-  private String value;
+  private static final Gson gson = new Gson();
+
+  private final String date;
+  private final String value;
 
   public DateValue(String date, String value) {
     this.date = date;
@@ -40,5 +45,11 @@ public class DateValue implements Serializable {
   @Override
   public String toString() {
     return String.format("DateValue{date='%s', value='%s'}", date, value);
+  }
+
+  public String toJsonString() {
+    Map<String, String> jsonMap = new HashMap<>();
+    jsonMap.put(this.date, this.value);
+    return gson.toJson(jsonMap);
   }
 }
---
+++ pipeline/ingestion/src/main/java/org/datacommons/ingestion/pipeline/SimpleObservationsPipelineOptions.java
@@ -0,0 +1,13 @@
+package org.datacommons.ingestion.pipeline;
+
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+
+/** Pipeline options for the SimpleObservationsPipeline. */
+public interface SimpleObservationsPipelineOptions extends BasePipelineOptions {
+  @Description("Import group to be ingested into Spanner.")
+  @Default.String("frequent")
+  String getImportGroup();
+
+  void setImportGroup(String importGroup);
+}
