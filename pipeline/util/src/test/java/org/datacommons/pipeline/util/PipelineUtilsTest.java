8086efb48063c3e00f01eaa21e0fedc6e41741d5
Vishal Gupta <vishg@google.com>
2025-08-06 06:10:56 +0000

    Fix logic to build optimized graph - Handle coder error - Add unit test

--- pipeline/util/src/test/java/org/datacommons/pipeline/util/PipelineUtilsTest.java
+++ pipeline/util/src/test/java/org/datacommons/pipeline/util/PipelineUtilsTest.java
@@ -1,7 +1,12 @@
 package org.datacommons.pipeline.util;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.beam.sdk.extensions.protobuf.ProtoCoder;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
@@ -12,6 +17,9 @@ import org.apache.beam.sdk.values.PCollection;
 import org.datacommons.proto.Mcf.McfGraph;
 import org.datacommons.proto.Mcf.McfGraph.PropertyValues;
 import org.datacommons.proto.Mcf.McfGraph.TypedValue;
+import org.datacommons.proto.Mcf.McfGraph.Values;
+import org.datacommons.proto.Mcf.McfOptimizedGraph;
+import org.datacommons.proto.Mcf.McfStatVarObsSeries;
 import org.datacommons.proto.Mcf.ValueType;
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,8 +30,185 @@ import org.junit.runners.JUnit4;
 public class PipelineUtilsTest {
 
   PipelineOptions options = PipelineOptionsFactory.create();
-
   @Rule public TestPipeline p = TestPipeline.fromOptions(options);
+
+  @Test
+  public void testBuildOptimizedMcfGraph() {
+
+    p.getCoderRegistry()
+        .registerCoderForClass(
+            McfStatVarObsSeries.Key.class, ProtoCoder.of(McfStatVarObsSeries.Key.class));
+
+    // Create two McfGraph objects representing observations for the same stat var.
+    McfGraph.Builder graph1 = McfGraph.newBuilder();
+    PropertyValues.Builder pv1 = PropertyValues.newBuilder();
+    pv1.putPvs(
+        "typeOf",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("StatVarObservation"))
+            .build());
+    pv1.putPvs(
+        "variableMeasured",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("count_person"))
+            .build());
+    pv1.putPvs(
+        "observationAbout",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("country/USA"))
+            .build());
+    pv1.putPvs(
+        "observationDate",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("2020")).build());
+    pv1.putPvs(
+        "value",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("32.0")).build());
+    pv1.putPvs(
+        "dcid",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("obs1")).build());
+    graph1.putNodes("obs1", pv1.build());
+
+    McfGraph.Builder graph2 = McfGraph.newBuilder();
+    PropertyValues.Builder pv2 = PropertyValues.newBuilder();
+    pv2.putPvs(
+        "typeOf",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("StatVarObservation"))
+            .build());
+    pv2.putPvs(
+        "variableMeasured",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("count_person"))
+            .build());
+    pv2.putPvs(
+        "observationAbout",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("country/USA"))
+            .build());
+    pv2.putPvs(
+        "observationDate",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("2021")).build());
+    pv2.putPvs(
+        "value",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("33.0")).build());
+    pv2.putPvs(
+        "dcid",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("obs2")).build());
+    graph2.putNodes("obs2", pv2.build());
+
+    PropertyValues.Builder pv3 = PropertyValues.newBuilder();
+    pv3.putPvs(
+        "typeOf",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("StatVarObservation"))
+            .build());
+    pv3.putPvs(
+        "variableMeasured",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("count_person"))
+            .build());
+    pv3.putPvs(
+        "observationAbout",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("country/USA"))
+            .build());
+    pv3.putPvs(
+        "observationDate",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("2022")).build());
+    pv3.putPvs(
+        "value",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("34.0")).build());
+    pv3.putPvs(
+        "dcid",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("obs3")).build());
+    graph2.putNodes("obs3", pv3.build());
+
+    PropertyValues.Builder pv4 = PropertyValues.newBuilder();
+    pv4.putPvs(
+        "typeOf",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("StatVarObservation"))
+            .build());
+    pv4.putPvs(
+        "variableMeasured",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("count_person"))
+            .build());
+    pv4.putPvs(
+        "observationAbout",
+        Values.newBuilder()
+            .addTypedValues(TypedValue.newBuilder().setValue("country/India"))
+            .build());
+    pv4.putPvs(
+        "observationDate",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("2022")).build());
+    pv4.putPvs(
+        "value",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("36.0")).build());
+    pv4.putPvs(
+        "dcid",
+        Values.newBuilder().addTypedValues(TypedValue.newBuilder().setValue("obs4")).build());
+    graph2.putNodes("obs4", pv4.build());
+    List<McfGraph> graphs = Arrays.asList(graph1.build(), graph2.build());
+    PCollection<McfGraph> input = p.apply(Create.of(graphs));
+
+    PCollection<McfOptimizedGraph> result = PipelineUtils.buildOptimizedMcfGraph(input);
+
+    // Expected output
+    McfStatVarObsSeries.Key.Builder keyBuilder = McfStatVarObsSeries.Key.newBuilder();
+    keyBuilder.setObservationAbout("country/USA");
+    keyBuilder.setVariableMeasured("count_person");
+
+    McfStatVarObsSeries.Key.Builder keyBuilder2 = McfStatVarObsSeries.Key.newBuilder();
+    keyBuilder2.setObservationAbout("country/India");
+    keyBuilder2.setVariableMeasured("count_person");
+
+    McfStatVarObsSeries.StatVarObs.Builder svObs1 = McfStatVarObsSeries.StatVarObs.newBuilder();
+    svObs1.setDate("2020");
+    svObs1.setNumber(32.0);
+    svObs1.setDcid("obs1");
+    svObs1.setPvs(PropertyValues.newBuilder().build());
+
+    McfStatVarObsSeries.StatVarObs.Builder svObs2 = McfStatVarObsSeries.StatVarObs.newBuilder();
+    svObs2.setDate("2021");
+    svObs2.setNumber(33.0);
+    svObs2.setDcid("obs2");
+    svObs2.setPvs(PropertyValues.newBuilder().build());
+
+    McfStatVarObsSeries.StatVarObs.Builder svObs3 = McfStatVarObsSeries.StatVarObs.newBuilder();
+    svObs3.setDate("2022");
+    svObs3.setNumber(34.0);
+    svObs3.setDcid("obs3");
+    svObs3.setPvs(PropertyValues.newBuilder().build());
+
+    McfStatVarObsSeries.StatVarObs.Builder svObs4 = McfStatVarObsSeries.StatVarObs.newBuilder();
+    svObs4.setDate("2022");
+    svObs4.setNumber(36.0);
+    svObs4.setDcid("obs4");
+    svObs4.setPvs(PropertyValues.newBuilder().build());
+
+    List<McfStatVarObsSeries.StatVarObs> sortedSvObs =
+        Arrays.asList(svObs1.build(), svObs2.build(), svObs3.build()).stream()
+            .sorted(Comparator.comparing(McfStatVarObsSeries.StatVarObs::getDate))
+            .collect(Collectors.toList());
+
+    McfStatVarObsSeries.Builder seriesBuilder = McfStatVarObsSeries.newBuilder();
+    seriesBuilder.setKey(keyBuilder.build());
+    seriesBuilder.addAllSvObsList(sortedSvObs);
+
+    McfStatVarObsSeries.Builder seriesBuilder2 = McfStatVarObsSeries.newBuilder();
+    seriesBuilder2.setKey(keyBuilder2.build());
+    seriesBuilder2.addAllSvObsList(List.of(svObs4.build()));
+
+    McfOptimizedGraph.Builder expectedGraph = McfOptimizedGraph.newBuilder();
+    expectedGraph.setSvObsSeries(seriesBuilder.build());
+
+    McfOptimizedGraph.Builder expectedGraph2 = McfOptimizedGraph.newBuilder();
+    expectedGraph2.setSvObsSeries(seriesBuilder2.build());
+
+    PAssert.that(result).containsInAnyOrder(expectedGraph.build(), expectedGraph2.build());
+    p.run().waitUntilFinish();
+  }
 
   @Test
   public void testCombineGraphNodes() {
