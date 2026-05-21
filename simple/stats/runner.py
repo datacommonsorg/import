a51b29a3a7ac2864a38fc681aa574517543c7d4c
Gabriel Mechali <gmechali@google.com>
2026-05-21 09:17:03 -0400

    Lint fix

--- simple/stats/runner.py
+++ simple/stats/runner.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime, timezone
+from datetime import datetime
+from datetime import timezone
 from enum import StrEnum
 import json
 import logging
