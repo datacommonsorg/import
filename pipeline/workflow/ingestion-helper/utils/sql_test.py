fc5d2ce9e24c562c33c6f8c5c24f37d29ca7d208
Gabriel Mechali <gabriel.mechali@gmail.com>
2026-07-07 10:11:41 -0400

    [DCP Ingestion] Update Ingestion History to have unique records per run and update the record by status / stage (#606)
    This PR adds an environment variable to contrl whether to keep the
    current schema, and behavior for writing to Ingesiton History, or use
    the new proposed approach.
    
    We use a flag to be able to move over DCP customers and Base DC safely
    with the new versions.
    
    The new proposal is for ingestionHistory to have unique records per run,
    and updates the status/stage thoroughly as it progresses through the
    workflow. This will allow a stronger visibility into the ingestion runs,
    and allow us to fix the issue with the computation of the stale
    timestamp

---
+++ pipeline/workflow/ingestion-helper/utils/sql_test.py
@@ -0,0 +1,56 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for SQL utility functions."""
+
+import unittest
+from utils.sql import parse_sql_to_statements
+
+class TestSqlUtils(unittest.TestCase):
+
+    def test_parse_sql_to_statements_basic(self):
+        sql = "CREATE TABLE A; CREATE TABLE B;"
+        statements = parse_sql_to_statements(sql)
+        self.assertEqual(statements, ["CREATE TABLE A", "CREATE TABLE B"])
+
+    def test_parse_sql_to_statements_with_comments(self):
+        sql = """
+        -- This is a comment; with a semicolon
+        CREATE TABLE A;
+        -- Another comment;
+        CREATE TABLE B (col INT64);
+        """
+        statements = parse_sql_to_statements(sql)
+        self.assertEqual(statements, [
+            "CREATE TABLE A",
+            "CREATE TABLE B (col INT64)"
+        ])
+
+    def test_parse_sql_to_statements_empty(self):
+        sql = "   ;  -- just comment; \n  ; "
+        statements = parse_sql_to_statements(sql)
+        self.assertEqual(statements, [])
+
+    def test_parse_sql_to_statements_with_trailing_comments(self):
+        sql = """
+        CREATE TABLE A; -- comment with semicolon; inside
+        CREATE TABLE B (col INT64); -- another trailing comment
+        """
+        statements = parse_sql_to_statements(sql)
+        self.assertEqual(statements, [
+            "CREATE TABLE A",
+            "CREATE TABLE B (col INT64)"
+        ])
+
+if __name__ == '__main__':
+    unittest.main()
